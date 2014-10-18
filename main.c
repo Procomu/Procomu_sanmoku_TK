bbb026944c8f0a39df2f9f5928e77b4e5fb6e695
Procomu <procomu.git@gmail.com>
2014-10-18 18:34:56 +0900

    bugfound

--- main.c
+++ main.c
@@ -2,12 +2,13 @@
 
 int Player;
 int Stone;
-int board[3][3];
+int board[5][5];
 char row;
-int temprow;
-int column;
+int tempRow;
+int column=-1;
 int putable;
 int judge;
+int tempStone;
 void initPlayer(){
 	 Player=0;
 }
@@ -15,16 +16,20 @@ void initStone(){
 	 Stone=1;
 }
 void initBoard(){
-	for(int i=0;i<3;i++){
-		for(int j=0;j<3;j++){
-			board[i][j]=8;
+	for(int i=0;i<5;i++){
+		for(int j=0;j<5;j++){
+			if(i==0||i==4||j==0||j==4){
+				board[i][j]=2;
+			}else{
+				board[i][j]=8;
+			}
 		}
-	}
+	}		
 }
 void showBoard(){
-	for(int i=0;i<3;i++){
+	for(int i=0;i<5;i++){
 		printf("\n");
-			for(int j=0;j<3;j++){
+			for(int j=0;j<5;j++){
 				printf("%d",board[i][j]);
 			}
 	}
@@ -34,7 +39,7 @@ void showBoard(){
 // comment by aira aziawa
 
 void putablestone(){
-	if(board[temprow][column]==8){
+	if(board[tempRow][column]==8){
 		putable=0;
 	}else{
 		putable=1;
@@ -46,13 +51,13 @@ void inputRow(){
 	row=getchar();
 	switch(row){
 		case'a':
-			temprow=0;
+			tempRow=0;
 			break;
 		case'b':
-			temprow=1;
+			tempRow=1;
 			break;
 		case'c':
-			temprow=2;
+			tempRow=2;
 			break;
 		default:
 		printf("please retry");
@@ -76,27 +81,87 @@ void inputColumn(){
 			break;
 	}
 	printf("\n");
-} 		
+} 
+void checkWin();
+	int i=tempRow;
+	int j=column;
+	if(board[--i][j]=Stone){
+		i=i-1;
+		j=j;
+		//????????????????????????
+		else if (board[--i][++j]=Stone){
+			i=i-1;
+			j=j+1;
+			//???????????????????????
+			else if(board[i][++j]=Stone){
+				i=i;
+				j=j+1;
+				//??????????????????????
+				else if(board[++i][++j]=Stone){
+				i=i+1;
+				j=j+1;
+				//??????????????????????
+					else if(board[++i][j]=Stone){
+					i=i+1;
+					j=j;
+					//??????????????????????
+						else if(board[++i][j--]=Stone){
+						i=i+1;
+						j=j-1;
+						//??????????????????????
+							else if(board[i][--j]=Stone){
+							i=i;
+							j=j-1;
+							//??????????????????????
+								else if(board[--i][--j]=Stone){
+								i=i-1;
+								j=j-1;
+								//??????????????????????
+			} 
+		}
+			
+			break
+		}
+	}
+
+void changeStone(){
+	printf("Stone change");
+	printf("\n");
+	Stone=Stone*-1;
+}
+void changePlayer(){
+	printf("Player change");
+	printf("\n");
+	if (Stone==1){Player=0;}
+	else if(Stone==-1){Player=1;}
+	printf("\n");
+	printf("Player=%d",Player);
+}
+ 		
 int main(int argc, char **argv)
 {
 	printf("Start Game");
+	printf("\n");
 	initPlayer();
 	initStone();
 	initBoard();
+	printf("Turn start with Player=%d",Player);
 	while(1==1){
 		showBoard();
 		inputRow();
 		inputColumn();
 		putablestone();
 		if(putable==0){
-			board[temprow][column]=Stone;
+			board[tempRow][column]=Stone;
 		}
+		changeStone();
+		changePlayer();
 	}
 	printf("\n");
 	printf("showPlayer=%d",Player);
 	printf("\n");
 	printf("showStone=%d",Stone);
-	printf("temprow=%d",temprow);
+	printf("tempRow=%d",tempRow);
 	printf("column=%d",column);
 	return 0;
 }
